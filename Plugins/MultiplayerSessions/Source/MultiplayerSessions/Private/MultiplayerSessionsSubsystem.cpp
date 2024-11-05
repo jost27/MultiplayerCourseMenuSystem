052957124aca31307a98c0445610e90a1119d610
jost27 <30935300+jost27@users.noreply.github.com>
2024-11-05 11:04:37 -0500

    Update MultiplayerSessionsSubsystem.cpp

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -5,6 +5,9 @@
 #include "OnlineSubsystem.h"
 #include "OnlineSessionSettings.h"
 #include "Online/OnlineSessionNames.h"
+#include "OnlineSubsystemUtils.h"
+#include "OnlineSubsystemTypes.h"
+#include "Interfaces/OnlineIdentityInterface.h"
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem():
 	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
@@ -13,8 +16,9 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem():
 	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
 	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
-	
+	bIsLogged = false;
 }
+
 
 void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
 {
@@ -45,6 +49,9 @@ void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FS
 	LastSessionSettings->bUsesPresence = true;
 	LastSessionSettings->Set(FName("MatchType"), MatchType, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 	LastSessionSettings->BuildUniqueId = 1;
+	
+ 
+	UE_LOG(LogTemp, Log, TEXT("Creating Lobby..."));
 
 	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
 	if (!SessionInterface->CreateSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *LastSessionSettings))
@@ -185,3 +192,100 @@ void UMultiplayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, 
 void UMultiplayerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
 {
 }
+
+void UMultiplayerSessionsSubsystem::Login()
+{
+    /*
+    Tutorial 2: This function will access the EOS OSS via the OSS identity interface to log first into Epic Account Services, and then into Epic Game Services.
+    It will bind a delegate to handle the callback event once login call succeeeds or fails. 
+    All functions that access the OSS will have this structure: 1-Get OSS interface, 2-Bind delegate for callback and 3-Call OSS interface function (which will call the correspongin EOS OSS function)
+    */
+    IOnlineSubsystem* Subsystem = Online::GetSubsystem(GetWorld());
+    IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface(); // This is the generic OSS interface that will access the EOS OSS.
+
+    // If you're logged in, don't try to login again.
+    // This can happen if your player travels to a dedicated server or different maps as BeginPlay() will be called each time.
+    
+    FUniqueNetIdPtr NetId = Identity->GetUniquePlayerId(0);
+
+    if (NetId != nullptr && Identity->GetLoginStatus(0) == ELoginStatus::LoggedIn)
+    {
+        return; 
+    }
+    
+    /* This binds a delegate so we can run our function when the callback completes. 0 represents the player number.
+    You should parametrize this Login function and pass the parameter here for splitscreen. 
+    */
+    LoginDelegateHandle = 
+	Identity->AddOnLoginCompleteDelegate_Handle(
+        0,
+        FOnLoginCompleteDelegate::CreateUObject(
+            this,
+            &ThisClass::HandleLoginCompleted));
+
+    // Grab command line parameters. If empty call hardcoded login function - Hardcoded login function useful for Play In Editor. 
+    FString AuthType; 
+    FParse::Value(FCommandLine::Get(), TEXT("AUTH_TYPE="), AuthType);
+
+    if (!AuthType.IsEmpty()) //If parameter is NOT empty we can autologin.
+    {
+        /* 
+        In most situations you will want to automatically log a player in using the parameters passed via CLI.
+        For example, using the exchange code for the Epic Games Store.
+        */
+        UE_LOG(LogTemp, Log, TEXT("Logging into EOS...")); // Log to the UE logs that we are trying to log in. 
+      
+        if (!Identity->AutoLogin(0))
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Failed to login... ")); // Log to the UE logs that we are trying to log in.
+			// Clear our handle and reset the delegate.
+			Identity->ClearOnLoginCompleteDelegate_Handle(0, LoginDelegateHandle);
+            LoginDelegateHandle.Reset();			
+        }
+    }
+    else
+    {
+        /* 
+        Fallback if the CLI parameters are empty.Useful for PIE.
+        The type here could be developer if using the DevAuthTool, ExchangeCode if the game is launched via the Epic Games Launcher, etc...
+        */
+        FOnlineAccountCredentials Credentials("AccountPortal","", "");
+
+        UE_LOG(LogTemp, Log, TEXT("Logging into EOS...")); // Log to the UE logs that we are trying to log in. 
+        
+        if (!Identity->Login(0, Credentials))
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Failed to login... ")); // Log to the UE logs that we are trying to log in.
+			// Clear our handle and reset the delegate. 
+            Identity->ClearOnLoginCompleteDelegate_Handle(0, LoginDelegateHandle);
+            LoginDelegateHandle.Reset();			
+        }        
+    }
+}
+
+
+
+void UMultiplayerSessionsSubsystem::HandleLoginCompleted(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error)
+{
+    /*
+    Tutorial 2: This function handles the callback from logging in. You should not proceed with any EOS features until this function is called.
+    This function will remove the delegate that was bound in the Login() function.
+    */
+    IOnlineSubsystem* Subsystem = Online::GetSubsystem(GetWorld());
+    IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface();
+    if (bWasSuccessful)
+    {
+        UE_LOG(LogTemp, Log, TEXT("Login callback completed!"));;
+    	bIsLogged = true;
+    }
+    else //Login failed
+    {
+        // If your game is online only, you may want to return an errror to the user and return to a menu that uses a different GameMode/PlayerController.
+
+        UE_LOG(LogTemp, Warning, TEXT("EOS login failed.")); //Print sign in failure in logs as a warning.
+    }
+
+    Identity->ClearOnLoginCompleteDelegate_Handle(LocalUserNum, LoginDelegateHandle);
+    LoginDelegateHandle.Reset();
+}
+
